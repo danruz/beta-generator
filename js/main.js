c574476d189b54858165bec619dfad84bd002620
Darwin Ruiz <36284562+danruz@users.noreply.github.com>
2023-08-04 16:25:12 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
- // From https://stackoverflow.com/a/4238971/259824
-
  function placeCaretAtEnd(el) {
     el.focus();
     if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
@@ -33,7 +31,7 @@
   }
   function cleanup(e) {
     const text = this.textContent;
-    if (text == 'Tunombre') {
+    if (text == 'TuNombre') {
       this.innerHTML = '';
       placeCaretAtEnd(this);
     }
@@ -42,7 +40,7 @@
     const target = e ? this : document.querySelector('h3');;
     const text = target.textContent;
     if (text === '') {
-      target.innerHTML = split('Tunombre');
+      target.innerHTML = split('TuNombre');
     }
   }
   function main(){
@@ -53,45 +51,4 @@
     initialize();
   }
   main();
-
-/* *** */
-    // create img & download
-  function downloadCanvas(canvasId, filename) {
-    // Obteniendo la etiqueta la cual se desea convertir en imagen
-    var domElement = document.getElementById(canvasId);
- 
-    // Utilizando la función html2canvas para hacer la conversión
-    html2canvas(domElement, {
-        onrendered: function(domElementCanvas) {
-            // Obteniendo el contexto del canvas ya generado
-            var context = domElementCanvas.getContext('2d');
- 
-            // Creando enlace para descargar la imagen generada
-            var link = document.createElement('a');
-            link.href = domElementCanvas.toDataURL("image/png");
-            link.download = filename;
- 
-            // Chequeando para browsers más viejos
-            if (document.createEvent) {
-                var event = document.createEvent('MouseEvents');
-                // Simulando clic para descargar
-                event.initMouseEvent("click", true, true, window, 0,
-                    0, 0, 0, 0,
-                    false, false, false, false,
-                    0, null);
-                link.dispatchEvent(event);
-            } else {
-                // Simulando clic para descargar
-                link.click();
-            }
-        }
-    });
-}
- 
-// Haciendo la conversión y descarga de la imagen al presionar el botón
-$('#boton-descarga').click(function() {
-    downloadCanvas('imagen', 'imagen.png');
-});
-
-
 
